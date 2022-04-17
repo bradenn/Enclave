f8a06240cc5178c7c1577e611a7c6b33999c392c
Braden <braden@bradenn.com>
2022-04-16 18:55:17 -0700

    Magical sauce or the server doesnt work.

--- src/main/java/com/bradenn/dev/enclave/Database.java
+++ src/main/java/com/bradenn/dev/enclave/Database.java
@@ -1,11 +1,16 @@
 package com.bradenn.dev.enclave;
 
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bson.codecs.UuidCodecProvider;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
 import org.bukkit.configuration.Configuration;
 
 import java.util.Objects;
@@ -39,9 +44,23 @@ public class Database {
         String uri = Objects.requireNonNull(config.getString("mongo.uri"));
         ConnectionString cS = new ConnectionString(uri);
 
-        return MongoClients.create(cS);
+        return MongoClients.create(buildMongoClientSettings(cS));
     }
 
+    private static MongoClientSettings buildMongoClientSettings(ConnectionString clusterUrl) {
+        return MongoClientSettings.builder()
+                .codecRegistry(codecRegistries())
+                .applyConnectionString(clusterUrl)
+                .build();
+    }
+
+    private static CodecRegistry codecRegistries() {
+        return CodecRegistries.fromRegistries(
+                // save uuids as UUID, instead of LUUID
+                CodecRegistries.fromProviders(new UuidCodecProvider(UuidRepresentation.STANDARD)),
+                MongoClientSettings.getDefaultCodecRegistry()
+        );
+    }
     public static MongoCollection<Document> getCollection(String collection) {
         return database.getCollection(collection);
     }
